c72245b8ea3aabfc2c2c33f2faade65bd1252f31
Peter Schrammel <peter.schrammel@experteer.com>
2011-07-18 12:38:39 +0200

    size is not supported (any more?); this works with rack 1.1.1

--- lib/rack/reverse_proxy.rb
+++ lib/rack/reverse_proxy.rb
@@ -37,7 +37,7 @@ module Rack
         when "PUT", "POST"
           req = Net::HTTP.const_get(m.capitalize).new(uri.request_uri, headers)
           req.basic_auth all_opts[:username], all_opts[:password] if all_opts[:username] and all_opts[:password]
-          req.content_length = rackreq.body.length
+          req.content_length = rackreq.body.size
           req.body_stream = rackreq.body
         else
           raise "method not supported: #{m}"
