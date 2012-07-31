10fe5feaa32848f7ed0b105f231c8a658b87444b
sinm <sinm.sinm@gmail.com>
2012-07-31 17:27:11 +0400

    Support for all the http methods in Net::HTTP

--- lib/rack/reverse_proxy.rb
+++ lib/rack/reverse_proxy.rb
@@ -38,14 +38,17 @@ module Rack
       end
       session.start { |http|
         m = rackreq.request_method
-        case m
-        when "GET", "HEAD", "DELETE", "OPTIONS", "TRACE"
-          req = Net::HTTP.const_get(m.capitalize).new(uri.request_uri, headers)
-          req.basic_auth all_opts[:username], all_opts[:password] if all_opts[:username] and all_opts[:password]
-        when "PUT", "POST"
-          req = Net::HTTP.const_get(m.capitalize).new(uri.request_uri, headers)
-          req.basic_auth all_opts[:username], all_opts[:password] if all_opts[:username] and all_opts[:password]
+        req = has_body = nil
+        begin
+          req = Net::HTTP.const_get(m.capitalize)
+          has_body = req::REQUEST_HAS_BODY
+        rescue
+          "method not supported: #{m}"
+        end
+        req = req.new(uri.request_uri, headers)
+        req.basic_auth all_opts[:username], all_opts[:password] if all_opts[:username] and all_opts[:password]
 
+        if has_body
           if rackreq.body.respond_to?(:read) && rackreq.body.respond_to?(:rewind)
             body = rackreq.body.read
             req.content_length = body.size
@@ -53,11 +56,8 @@ module Rack
           else
             req.content_length = rackreq.body.size
           end
-
           req.content_type = rackreq.content_type unless rackreq.content_type.nil?
           req.body_stream = rackreq.body
-        else
-          raise "method not supported: #{m}"
         end
 
         body = ''
