37b25ee759eea9be8c6ddbf7d88714549ad92784
pex <rernst@farbenmeer.net>
2014-09-16 00:07:38 +0200

    Take first response_headers location when replacing host

--- lib/rack/reverse_proxy.rb
+++ lib/rack/reverse_proxy.rb
@@ -79,7 +79,7 @@ module Rack
 
       # Replace the location header with the proxy domain
       if response_headers['location'] && options[:replace_response_host]
-        response_location = URI(response_headers['location'])
+        response_location = URI(response_headers['location'][0])
         response_location.host = source_request.host
         response_headers['location'] = response_location.to_s
       end
