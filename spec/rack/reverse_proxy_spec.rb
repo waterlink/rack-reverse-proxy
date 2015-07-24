092e3cc5fe27dc5bc40efbc2c8f7d1809c461cdb
Jan Raasch <jan@janraasch.com>
2015-07-24 10:42:22 +0200

    Remove Rack::HttpStreamingResponse monkey-patch by upgrading rack-proxy
    - In v0.5.14 Rack::HttpStreamingResponse#read_timeout= was added, see https://github.com/ncr/rack-proxy/commit/42e32e37bbbb9117cbd08e5a7ad8e41af8858973
    - Easier to upgrade dependency than to maintain patching of external dependency

--- spec/rack/reverse_proxy_spec.rb
+++ spec/rack/reverse_proxy_spec.rb
@@ -145,7 +145,7 @@ RSpec.describe Rack::ReverseProxy do
 
       it "should make request with basic auth" do
         stub_request(:get, "http://example.com/test/slow")
-        Rack::HttpStreamingResponse.any_instance.should_receive(:set_read_timeout).with(99)
+        Rack::HttpStreamingResponse.any_instance.should_receive(:read_timeout=).with(99)
         get '/test/slow'
       end
     end
@@ -159,7 +159,7 @@ RSpec.describe Rack::ReverseProxy do
 
       it "should make request with basic auth" do
         stub_request(:get, "http://example.com/test/slow")
-        Rack::HttpStreamingResponse.any_instance.should_not_receive(:set_read_timeout)
+        Rack::HttpStreamingResponse.any_instance.should_not_receive(:read_timeout=)
         get '/test/slow'
       end
     end
