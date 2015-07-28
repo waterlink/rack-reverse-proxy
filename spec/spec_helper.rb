c8083a275b53c1fe3fdfe28e1151171748d12c85
Oleksii Fedorov <waterlink000@gmail.com>
2015-07-28 09:28:06 +0200

    Fix jruby + rspec-mocks + HttpStreamingResponse + read_timeout= problem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,12 +13,12 @@ RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4.
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-    expectations.syntax = [:should]
+    expectations.syntax = [:should, :expect]
   end
   config.mock_with :rspec do |mocks|
     mocks.verify_doubled_constant_names = true
     mocks.verify_partial_doubles = true
-    mocks.syntax = [:should]
+    mocks.syntax = [:should, :expect]
     # Prevents you from mocking or stubbing a method that does not exist on
     # a real object. This is generally recommended, and will default to
     # `true` in RSpec 4.
