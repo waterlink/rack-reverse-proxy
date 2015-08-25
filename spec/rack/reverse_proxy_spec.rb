12673459e8d6814863b726170aa4d83ca61ee0a4
Oleksii Fedorov <waterlink000@gmail.com>
2015-08-25 23:15:34 +0300

    Upgrade specs syntax

--- spec/rack/reverse_proxy_spec.rb
+++ spec/rack/reverse_proxy_spec.rb
@@ -30,76 +30,76 @@ RSpec.describe Rack::ReverseProxy do
       end
     end
 
-    it "should forward requests to the calling app when the path is not matched" do
+    it "forwards requests to the calling app when the path is not matched" do
       get '/'
-      last_response.body.should == "Dummy App"
-      last_response.should be_ok
+      expect(last_response.body).to eq("Dummy App")
+      expect(last_response).to be_ok
     end
 
-    it "should proxy requests when a pattern is matched" do
+    it "proxies requests when a pattern is matched" do
       stub_request(:get, 'http://example.com/test').to_return({:body => "Proxied App"})
       get '/test'
-      last_response.body.should == "Proxied App"
+      expect(last_response.body).to eq("Proxied App")
     end
 
-    it "should produce a response header of type HeaderHash" do
+    it "produces a response header of type HeaderHash" do
       stub_request(:get, 'http://example.com/test')
       get '/test'
-      last_response.headers.should be_an_instance_of Rack::Utils::HeaderHash
+      expect(last_response.headers).to be_an_instance_of(Rack::Utils::HeaderHash)
     end
 
-    it "should parse the headers as a Hash with values of type String" do
+    it "parses the headers as a Hash with values of type String" do
       stub_request(:get, 'http://example.com/test').to_return({:headers => {'cache-control'=> 'max-age=300, public'} })
       get '/test'
-      last_response.headers['cache-control'].should be_an_instance_of String
-      last_response.headers['cache-control'].should == 'max-age=300, public'
+      expect(last_response.headers['cache-control']).to be_an_instance_of(String)
+      expect(last_response.headers['cache-control']).to eq('max-age=300, public')
     end
 
-    it "should proxy requests to a lambda url when a pattern is matched" do
+    it "proxies requests to a lambda url when a pattern is matched" do
       stub_request(:get, 'http://example.com/2test').to_return({:body => "Proxied App2"})
       get '/2test'
-      last_response.body.should == "Proxied App2"
+      expect(last_response.body).to eq("Proxied App2")
     end
 
-    it "should set the Host header w/o default port" do
+    it "sets the Host header w/o default port" do
       stub_request(:any, 'example.com/test/stuff')
       get '/test/stuff'
-      a_request(:get, 'http://example.com/test/stuff').with(:headers => {"Host" => "example.com"}).should have_been_made
+      expect(a_request(:get, 'http://example.com/test/stuff').with(:headers => {"Host" => "example.com"})).to have_been_made
     end
 
-    it "should set the X-Forwarded-Host header to the proxying host by default" do
+    it "sets the X-Forwarded-Host header to the proxying host by default" do
       stub_request(:any, 'example.com/test/stuff')
       get '/test/stuff'
-      a_request(:get, 'http://example.com/test/stuff').with(:headers => {'X-Forwarded-Host' => 'example.org'}).should have_been_made
+      expect(a_request(:get, 'http://example.com/test/stuff').with(:headers => {'X-Forwarded-Host' => 'example.org'})).to have_been_made
     end
 
-    it 'should should not produce headers with a Status key' do
+    it 'does not produce headers with a Status key' do
       stub_request(:get, 'http://example.com/2test').to_return({:status => 301, :headers => {:status => '301 Moved Permanently'}})
 
       get '/2test'
 
       headers = last_response.headers.to_hash
-      headers['Status'].should be_nil
+      expect(headers['Status']).to be_nil
     end
 
-    it 'should format the headers correctly to avoid duplicates' do
+    it 'formats the headers correctly to avoid duplicates' do
       stub_request(:get, 'http://example.com/2test').to_return(:headers => {:date => 'Wed, 22 Jul 2015 11:27:21 GMT'})
 
       get '/2test'
 
       headers = last_response.headers.to_hash
-      headers['Date'].should == 'Wed, 22 Jul 2015 11:27:21 GMT'
-      headers['date'].should be_nil
+      expect(headers['Date']).to eq('Wed, 22 Jul 2015 11:27:21 GMT')
+      expect(headers['date']).to be_nil
     end
 
-    it 'should format the headers with dashes correctly' do
+    it 'formats the headers with dashes correctly' do
       stub_request(:get, 'http://example.com/2test').to_return({:status => 301, :headers => {:status => '301 Moved Permanently', :"x-additional-info" => "something"}})
 
       get '/2test'
 
       headers = last_response.headers.to_hash
-      headers['X-Additional-Info'].should == "something"
-      headers['x-additional-info'].should be_nil
+      expect(headers['X-Additional-Info']).to eq("something")
+      expect(headers['x-additional-info']).to be_nil
     end
 
     describe "with non-default port" do
@@ -109,10 +109,10 @@ RSpec.describe Rack::ReverseProxy do
         end
       end
 
-      it "should set the Host header including non-default port" do
+      it "sets the Host header including non-default port" do
         stub_request(:any, 'example.com:8080/test/stuff')
         get '/test/stuff'
-        a_request(:get, 'http://example.com:8080/test/stuff').with(:headers => {"Host" => "example.com:8080"}).should have_been_made
+        expect(a_request(:get, 'http://example.com:8080/test/stuff').with(:headers => {"Host" => "example.com:8080"})).to have_been_made
       end
     end
 
@@ -123,11 +123,11 @@ RSpec.describe Rack::ReverseProxy do
         end
       end
 
-      it "should not set the Host header" do
+      it "does not set the Host header" do
         stub_request(:any, 'example.com/test/stuff')
         get '/test/stuff'
-        a_request(:get, 'http://example.com/test/stuff').with(:headers => {"Host" => "example.com"}).should_not have_been_made
-        a_request(:get, 'http://example.com/test/stuff').should have_been_made
+        expect(a_request(:get, 'http://example.com/test/stuff').with(:headers => {"Host" => "example.com"})).not_to have_been_made
+        expect(a_request(:get, 'http://example.com/test/stuff')).to have_been_made
       end
     end
 
@@ -139,11 +139,11 @@ RSpec.describe Rack::ReverseProxy do
         end
       end
 
-      it "should not set the X-Forwarded-Host header to the proxying host" do
+      it "does not set the X-Forwarded-Host header to the proxying host" do
         stub_request(:any, 'example.com/test/stuff')
         get '/test/stuff'
-        a_request(:get, 'http://example.com/test/stuff').with(:headers => {'X-Forwarded-Host' => 'example.org'}).should_not have_been_made
-        a_request(:get, 'http://example.com/test/stuff').should have_been_made
+        expect(a_request(:get, 'http://example.com/test/stuff').with(:headers => {'X-Forwarded-Host' => 'example.org'})).not_to have_been_made
+        expect(a_request(:get, 'http://example.com/test/stuff')).to have_been_made
       end
     end
 
@@ -154,7 +154,7 @@ RSpec.describe Rack::ReverseProxy do
         end
       end
 
-      it "should make request with basic auth" do
+      it "makes request with basic auth" do
         stub_request(:get, "http://example.com/test/slow")
         allow(Rack::HttpStreamingResponse).to receive(:new).and_return(http_streaming_response)
         expect(http_streaming_response).to receive(:read_timeout=).with(99)
@@ -169,7 +169,7 @@ RSpec.describe Rack::ReverseProxy do
         end
       end
 
-      it "should make request with basic auth" do
+      it "makes request with basic auth" do
         stub_request(:get, "http://example.com/test/slow")
         allow(Rack::HttpStreamingResponse).to receive(:new).and_return(http_streaming_response)
         expect(http_streaming_response).not_to receive(:read_timeout=)
@@ -184,10 +184,10 @@ RSpec.describe Rack::ReverseProxy do
         end
       end
 
-      it "should make request with basic auth" do
+      it "makes request with basic auth" do
         stub_request(:get, "http://joe:shmoe@example.com/test/stuff").to_return(:body => "secured content")
         get '/test/stuff'
-        last_response.body.should == "secured content"
+        expect(last_response.body).to eq("secured content")
       end
     end
 
@@ -198,16 +198,16 @@ RSpec.describe Rack::ReverseProxy do
         end
       end
 
-      it "should replace the location response header" do
+      it "replaces the location response header" do
         stub_request(:get, "http://example.com/test/stuff").to_return(:headers => {"location" => "http://test.com/bar"})
         get 'http://example.com/test/stuff'
-        last_response.headers['location'].should == "http://example.com/bar"
+        expect(last_response.headers['location']).to eq("http://example.com/bar")
       end
 
-      it "should keep the port of the location" do
+      it "keeps the port of the location" do
         stub_request(:get, "http://example.com/test/stuff").to_return(:headers => {"location" => "http://test.com/bar"})
         get 'http://example.com:3000/test/stuff'
-        last_response.headers['location'].should == "http://example.com:3000/bar"
+        expect(last_response.headers['location']).to eq("http://example.com:3000/bar")
       end
     end
 
@@ -220,11 +220,12 @@ RSpec.describe Rack::ReverseProxy do
         end
       end
 
-      it "should throw an exception" do
-        lambda { get '/test' }.should raise_error(RackReverseProxy::Errors::AmbiguousMatch)
+      it "raises an exception" do
+        expect { get '/test' }.to raise_error(RackReverseProxy::Errors::AmbiguousMatch)
       end
     end
 
+    # FIXME: descriptions are not consistent with examples
     describe "with ambiguous routes and first matching" do
       def app
         Rack::ReverseProxy.new(dummy_app) do
@@ -234,10 +235,10 @@ RSpec.describe Rack::ReverseProxy do
         end
       end
 
-      it "should throw an exception" do
+      it "raises an exception" do
         stub_request(:get, 'http://example1.com/test').to_return({:body => "Proxied App"})
         get '/test'
-        last_response.body.should == "Proxied App"
+        expect(last_response.body).to eq("Proxied App")
       end
     end
 
@@ -248,10 +249,10 @@ RSpec.describe Rack::ReverseProxy do
         end
       end
 
-      it "should support subcaptures" do
+      it "supports subcaptures" do
         stub_request(:get, 'http://example.com/path').to_return({:body => "Proxied App"})
         get '/test/path'
-        last_response.body.should == "Proxied App"
+        expect(last_response.body).to eq("Proxied App")
       end
     end
 
@@ -262,16 +263,16 @@ RSpec.describe Rack::ReverseProxy do
         end
       end
 
-      it "should make a secure request" do
+      it "makes a secure request" do
         stub_request(:get, 'https://example.com/test/stuff').to_return({:body => "Proxied Secure App"})
         get '/test/stuff'
-        last_response.body.should == "Proxied Secure App"
+        expect(last_response.body).to eq("Proxied Secure App")
       end
 
-      it "should set the Host header w/o default port" do
+      it "sets the Host header w/o default port" do
         stub_request(:any, 'https://example.com/test/stuff')
         get '/test/stuff'
-        a_request(:get, 'https://example.com/test/stuff').with(:headers => {"Host" => "example.com"}).should have_been_made
+        expect(a_request(:get, 'https://example.com/test/stuff').with(:headers => {"Host" => "example.com"})).to have_been_made
       end
     end
 
@@ -282,10 +283,10 @@ RSpec.describe Rack::ReverseProxy do
         end
       end
 
-      it "should set the Host header including non-default port" do
+      it "sets the Host header including non-default port" do
         stub_request(:any, 'https://example.com:8443/test/stuff')
         get '/test/stuff'
-        a_request(:get, 'https://example.com:8443/test/stuff').with(:headers => {"Host" => "example.com:8443"}).should have_been_made
+        expect(a_request(:get, 'https://example.com:8443/test/stuff').with(:headers => {"Host" => "example.com:8443"})).to have_been_made
       end
     end
 
@@ -297,10 +298,10 @@ RSpec.describe Rack::ReverseProxy do
         end
       end
 
-      it "should append the full path to the uri" do
+      it "appends the full path to the uri" do
         stub_request(:get, 'http://example.com/test/stuff').to_return({:body => "Proxied App"})
         get '/test/stuff'
-        last_response.body.should == "Proxied App"
+        expect(last_response.body).to eq("Proxied App")
       end
 
     end
@@ -312,8 +313,8 @@ RSpec.describe Rack::ReverseProxy do
         end
       end
 
-      it "should throw an exception" do
-        lambda{ app }.should raise_error(RackReverseProxy::Errors::GenericURI)
+      it "throws an exception" do
+        expect { app }.to raise_error(RackReverseProxy::Errors::GenericURI)
       end
     end
 
@@ -326,17 +327,17 @@ RSpec.describe Rack::ReverseProxy do
 
       %w|get head delete put post|.each do |method|
         describe "and using method #{method}" do
-          it "should forward the correct request" do
+          it "forwards the correct request" do
             stub_request(method.to_sym, 'http://example.com/test').to_return({:body => "Proxied App for #{method}"})
-            eval "#{method} '/test'"
-            last_response.body.should == "Proxied App for #{method}"
+            send(method, '/test')
+            expect(last_response.body).to eq("Proxied App for #{method}")
           end
 
           if %w|put post|.include?(method)
-            it "should forward the request payload" do
+            it "forwards the request payload" do
               stub_request(method.to_sym, 'http://example.com/test').to_return { |req| {:body => req.body} }
-              eval "#{method} '/test', {:test => 'test'}"
-              last_response.body.should == "test=test"
+              send(method, '/test', {:test => 'test'})
+              expect(last_response.body).to eq("test=test")
             end
           end
         end
@@ -366,19 +367,21 @@ RSpec.describe Rack::ReverseProxy do
         end
       end
 
-      it "should forward requests to the calling app when the path is not matched" do
+      it "forwards requests to the calling app when the path is not matched" do
         get '/'
-        last_response.body.should == "Dummy App"
-        last_response.should be_ok
+        expect(last_response.body).to eq("Dummy App")
+        expect(last_response).to be_ok
       end
 
-      it "should proxy requests when a pattern is matched" do
+      it "proxies requests when a pattern is matched" do
         stub_request(:get, 'http://example.com/test').to_return({:body => "Proxied App"})
         stub_request(:get, 'http://users-example.com/users').to_return({:body => "User App"})
+
         get '/test'
-        last_response.body.should == "Proxied App"
+        expect(last_response.body).to eq("Proxied App")
+
         get '/users'
-        last_response.body.should == "User App"
+        expect(last_response.body).to eq("User App")
       end
     end
 
@@ -409,21 +412,22 @@ RSpec.describe Rack::ReverseProxy do
         end
       end
 
-      it "should forward requests to the calling app when the path is not matched" do
+      it "forwards requests to the calling app when the path is not matched" do
         get '/'
-        last_response.body.should == "Dummy App"
-        last_response.should be_ok
+        expect(last_response.body).to eq("Dummy App")
+        expect(last_response).to be_ok
       end
 
-      it "should proxy requests when a pattern is matched" do
+      it "proxies requests when a pattern is matched" do
         stub_request(:get, 'http://users-example.com/users?user=omer').to_return({:body => "User App"})
+
         get '/test', :user => "mark"
-        last_response.body.should == "Dummy App"
+        expect(last_response.body).to eq("Dummy App")
+
         get '/users', :user => 'omer'
-        last_response.body.should == "User App"
+        expect(last_response.body).to eq("User App")
       end
     end
-
 
     describe "with a matching class that accepts headers" do
       class MatcherHeaders
@@ -444,24 +448,24 @@ RSpec.describe Rack::ReverseProxy do
         end
       end
 
-      it "should proxy requests when a pattern is matched and correct headers are passed" do
+      it "proxies requests when a pattern is matched and correct headers are passed" do
         stub_request(:get, 'http://example.com/test').to_return({:body => "Proxied App with Headers"})
         get '/test', {}, {'HTTP_ACCEPT' => 'foo.bar'}
-        last_response.body.should == "Proxied App with Headers"
+        expect(last_response.body).to eq("Proxied App with Headers")
       end
 
-      it "should not proxy requests when a pattern is matched and incorrect headers are passed" do
+      it "does not proxy requests when a pattern is matched and incorrect headers are passed" do
         stub_request(:get, 'http://example.com/test').to_return({:body => "Proxied App with Headers"})
         get '/test', {}, {'HTTP_ACCEPT' => 'bar.foo'}
-        last_response.body.should_not == "Proxied App with Headers"
+        expect(last_response.body).not_to eq("Proxied App with Headers")
       end
     end
   end
 
   describe "as a rack app" do
-    it "should respond with 404 when the path is not matched" do
+    it "responds with 404 when the path is not matched" do
       get '/'
-      last_response.should be_not_found
+      expect(last_response).to be_not_found
     end
   end
 
